eae4a7fe72ff3a18c2fb1dc60c3be197381ff6b4
Stars1233 <167130192+Stars1233@users.noreply.github.com>
2024-07-03 13:46:33 +0300

    Initialize crosssigning_bootstrap

---
+++ Riot/Managers/Settings/RiotSettings.swift
@@ -0,0 +1,156 @@
+/*
+ Copyright 2018 New Vector Ltd
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import Foundation
+
+/// Store Riot specific app settings.
+@objcMembers
+final class RiotSettings: NSObject {
+    
+    // MARK: - Constants
+    
+    private enum UserDefaultsKeys {
+        static let enableCrashReport = "enableCrashReport"
+        static let enableRageShake = "enableRageShake"
+        static let createConferenceCallsWithJitsi = "createConferenceCallsWithJitsi"
+        static let userInterfaceTheme = "userInterfaceTheme"
+        static let notificationsShowDecryptedContent = "showDecryptedContent"
+        static let pinRoomsWithMissedNotifications = "pinRoomsWithMissedNotif"
+        static let pinRoomsWithUnreadMessages = "pinRoomsWithUnread"
+        static let allowStunServerFallback = "allowStunServerFallback"
+        static let stunServerFallback = "stunServerFallback"
+        static let hideVerifyThisSessionAlert = "hideVerifyThisSessionAlert"
+        static let hideReviewSessionsAlert = "hideReviewSessionsAlert"
+    }
+    
+    static let shared = RiotSettings()
+    
+    // MARK: - Public
+    
+    // MARK: Notifications
+    
+    /// Indicate if `showDecryptedContentInNotifications` settings has been set once.
+    var isShowDecryptedContentInNotificationsHasBeenSetOnce: Bool {
+        return UserDefaults.standard.object(forKey: UserDefaultsKeys.notificationsShowDecryptedContent) != nil
+    }
+    
+    /// Indicate if encrypted messages content should be displayed in notifications.
+    var showDecryptedContentInNotifications: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: UserDefaultsKeys.notificationsShowDecryptedContent)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.notificationsShowDecryptedContent)
+        }
+    }
+    
+    /// Indicate if rooms with missed notifications should be displayed first on home screen.
+    var pinRoomsWithMissedNotificationsOnHome: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: UserDefaultsKeys.pinRoomsWithMissedNotifications)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.pinRoomsWithMissedNotifications)
+        }
+    }
+    
+    /// Indicate if rooms with unread messages should be displayed first on home screen.
+    var pinRoomsWithUnreadMessagesOnHome: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: UserDefaultsKeys.pinRoomsWithUnreadMessages)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.pinRoomsWithUnreadMessages)
+        }
+    }
+    
+    // MARK: User interface
+    
+    var userInterfaceTheme: String? {
+        get {
+            return UserDefaults.standard.string(forKey: UserDefaultsKeys.userInterfaceTheme)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.userInterfaceTheme)
+        }
+    }
+    
+    // MARK: Other
+    
+    /// Indicate if `enableCrashReport` settings has been set once.
+    var isEnableCrashReportHasBeenSetOnce: Bool {
+        return UserDefaults.standard.object(forKey: UserDefaultsKeys.enableCrashReport) != nil
+    }
+    
+    var enableCrashReport: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: UserDefaultsKeys.enableCrashReport)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.enableCrashReport)
+        }
+    }
+    
+    var enableRageShake: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: UserDefaultsKeys.enableRageShake)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.enableRageShake)
+        }
+    }
+    
+    // MARK: Labs
+    
+    var createConferenceCallsWithJitsi: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: UserDefaultsKeys.createConferenceCallsWithJitsi)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.createConferenceCallsWithJitsi)
+        }
+    }
+
+    // MARK: Calls
+
+    /// Indicate if `allowStunServerFallback` settings has been set once.
+    var isAllowStunServerFallbackHasBeenSetOnce: Bool {
+        return UserDefaults.standard.object(forKey: UserDefaultsKeys.allowStunServerFallback) != nil
+    }
+
+    var allowStunServerFallback: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: UserDefaultsKeys.allowStunServerFallback)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.allowStunServerFallback)
+        }
+    }
+
+    var stunServerFallback: String? {
+        return UserDefaults.standard.string(forKey: UserDefaultsKeys.stunServerFallback)
+    }
+    
+    // MARK: Key verification
+    
+    var hideVerifyThisSessionAlert: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: UserDefaultsKeys.hideVerifyThisSessionAlert)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.hideVerifyThisSessionAlert)
+        }
+    }
+    
+    var hideReviewSessionsAlert: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: UserDefaultsKeys.hideReviewSessionsAlert)
+        } set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.hideReviewSessionsAlert)
+        }
+    }
+}
---
+++ Riot/Modules/Room/CellData/RoomBubbleCellData.h
@@ -0,0 +1,112 @@
+/*
+ Copyright 2015 OpenMarket Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <MatrixKit/MatrixKit.h>
+
+// Custom tags for MXKRoomBubbleCellDataStoring.tag
+typedef NS_ENUM(NSInteger, RoomBubbleCellDataTag)
+{
+    RoomBubbleCellDataTagMessage = 0, // Default value used for messages
+    RoomBubbleCellDataTagMembership,
+    RoomBubbleCellDataTagRoomCreateWithPredecessor,
+    RoomBubbleCellDataTagKeyVerificationNoDisplay,
+    RoomBubbleCellDataTagKeyVerificationRequestIncomingApproval,
+    RoomBubbleCellDataTagKeyVerificationRequest,
+    RoomBubbleCellDataTagKeyVerificationConclusion
+};
+
+/**
+ `RoomBubbleCellData` defines Vector bubble cell data model.
+ */
+@interface RoomBubbleCellData : MXKRoomBubbleCellDataWithAppendingMode
+
+/**
+ A Boolean value that determines whether this bubble contains the current last message.
+ Used to keep displaying the timestamp of the last message.
+ */
+@property(nonatomic) BOOL containsLastMessage;
+
+/**
+ Indicate true to display the timestamp of the selected component.
+ */
+@property(nonatomic) BOOL showTimestampForSelectedComponent;
+
+/**
+ Indicate true to display the timestamp of the selected component on the left if possible (YES by default).
+ */
+@property(nonatomic) BOOL displayTimestampForSelectedComponentOnLeftWhenPossible;
+
+/**
+ The event id of the current selected event inside the bubble. Default is nil.
+ */
+@property(nonatomic) NSString *selectedEventId;
+
+/**
+ The index of the oldest component (component with a timestamp, and an actual display). NSNotFound by default.
+ */
+@property(nonatomic, readonly) NSInteger oldestComponentIndex;
+
+/**
+ The index of the most recent component (component with a timestamp, and an actual display). NSNotFound by default.
+ */
+@property(nonatomic, readonly) NSInteger mostRecentComponentIndex;
+
+/**
+ The index of the current selected component. NSNotFound by default.
+ */
+@property(nonatomic, readonly) NSInteger selectedComponentIndex;
+
+/**
+ Return additional content height (read receipts, reactions).
+ */
+@property(nonatomic, readonly) CGFloat additionalContentHeight;
+
+/**
+ MXKeyVerification object associated to key verifcation event when using key verification by direct message.
+ */
+@property(nonatomic, strong) MXKeyVerification *keyVerification;
+
+/**
+ Indicate if there is a pending operation that updates `keyVerification` property.
+ */
+@property(nonatomic) BOOL isKeyVerificationOperationPending;
+
+/**
+ Indicate to update additional content height.
+ */
+- (void)setNeedsUpdateAdditionalContentHeight;
+
+/**
+ Update additional content height if needed.
+ */
+- (void)updateAdditionalContentHeightIfNeeded;
+
+/**
+ The index of the first visible component. NSNotFound by default.
+ */
+- (NSInteger)firstVisibleComponentIndex;
+
+#pragma mark - Show all reactions
+
+- (BOOL)showAllReactionsForEvent:(NSString*)eventId;
+- (void)setShowAllReactions:(BOOL)showAllReactions forEvent:(NSString*)eventId;
+
+
+#pragma mark - Accessibility
+
+- (NSString*)accessibilityLabel;
+
+@end
